0b14f0b24f230c98f1267505ab477dca9a8688e8
thanhduong1311 <thanhduongjnguyen@gmail.com>
2024-09-20 01:47:45 +0700

    edit auth controller, edit endpoint

--- backend/VEMSBackEnd/VemsApi/Services/AccountService.cs
+++ backend/VEMSBackEnd/VemsApi/Services/AccountService.cs
@@ -1,6 +1,7 @@
 using BusinessObject;
 using DataAccess.DTO;
 using DataAccess.Repository;
+using SchoolMate.Dto.AuthenticationDto;
 
 public interface IAccountService
 {
@@ -8,6 +9,9 @@ public interface IAccountService
     Task<List<Teacher>> GetAllTeacherAccountAsync();
     Task<List<Student>> GetAllStudentAccountAsync();
     Task<CommonAccountType> GetAccountByIdAsync(Guid accountID);
+    Task<bool> ChangePassword(ChangePasswordRequest usernameOrEmail);
+    Task<List<Student>> RegisterStudent(List<RegisterStudentRequest> request);
+    Task<List<Teacher>> RegisterTeacher(List<RegisterTeacherRequest> request);
 
 }
 
@@ -39,5 +43,58 @@ public class AccountService : IAccountService
     public async Task<CommonAccountType> GetAccountByIdAsync(Guid accountID)
     {
         return await _accountRepository.GetAccountByIDAsync(accountID);
+    }
+
+    public Task<bool> ChangePassword(ChangePasswordRequest request)
+    {
+        return _accountRepository.UpdatePassword(request.AccountID, Hashing(request.NewPassword));
+    }
+
+    public string Hashing(string password)
+    {
+        return BCrypt.Net.BCrypt.HashPassword(password, 6);
+    }
+
+    public async Task<List<Student>> RegisterStudent(List<RegisterStudentRequest> request)
+    {
+        List<Student> newStudentList = new List<Student>();
+
+        foreach (var student in request)
+        {
+            Student newStudent = new Student
+            {
+                Username = student.PublicStudentID,
+                PublicStudentID = student.PublicStudentID,
+                FullName = student.FullName,
+                RoleId = student.RoleId,
+                ClassroomId = student.ClassroomId,
+                Password = Hashing("1")
+            };
+            newStudentList.Add(newStudent);
+        }
+
+        return await this._accountRepository.RegisterStudentAsync(newStudentList);
+    }
+
+    public async Task<List<Teacher>> RegisterTeacher(List<RegisterTeacherRequest> request)
+    {
+        List<Teacher> newTeacherList = new List<Teacher>();
+
+        foreach (var teacher in request)
+        {
+            Teacher newTeacher = new Teacher
+            {
+                Username = teacher.Phone,
+                Phone = teacher.Phone,
+                Email = teacher.Email,
+                FullName = teacher.FullName,
+                RoleId = teacher.RoleId,
+                //ClassroomId = student.ClassroomId,
+                Password = Hashing("1")
+            };
+            newTeacherList.Add(newTeacher);
+        }
+
+        return await this._accountRepository.RegisterTeacherAsync(newTeacherList);
     }
 }
